e158426329425ccc22479c8bd499f6cee4821b58
taras <tviyatyk@gmail.com>
2020-01-18 15:29:52 +0200

    Fix sign-up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,4 @@
 Devise.setup do |config|
-  # The e-mail address that mail will appear to be sent from
-  # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
-  config.mailer_sender = "support@myapp.com"
-
-  # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
@@ -13,4 +8,7 @@ Devise.setup do |config|
   # middleware b/c rails-api does not include it.
   # See: https://stackoverflow.com/q/19600905/806956
   config.navigational_formats = [:json]
+
+  # To avoid initial email confirming when user sign-in first time.
+  config.allow_unconfirmed_access_for = nil
 end
